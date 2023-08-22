b26497cf59803df0b446524a085bd69df28756b4
kiplangatwayne <waynekiplangatkorir903@gmail.com>
2023-08-22 15:35:04 +0300

    completed Assingment

--- lib/dog.py
+++ lib/dog.py
@@ -5,3 +5,14 @@ class Dog:
 
     #Instance method definition
     pass
+class Dog:
+    def bark(self):
+        print("Woof!")
+
+    def sit(self):
+        print("The dog is sitting.")
+
+# Testing the Dog class
+fido = Dog()
+fido.bark()  # Outputs: Woof!
+fido.sit()   # Outputs: The dog is sitting.
--- lib/person.py
+++ lib/person.py
@@ -5,3 +5,14 @@ class Person:
 
     #Instance method definition
     pass
+class Person:
+    def talk(self):
+        print("Hello World!")
+
+    def walk(self):
+        print("The person is walking.")
+
+# Testing the Person class
+alice = Person()
+alice.talk()  # Outputs: Hello World!
+alice.walk()  # Outputs: The person is walking.
